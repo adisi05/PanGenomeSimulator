04bb416c90b61ab57c73c492c500b1abc65563fb
adisi05-lab <adi.sivan05@gmail.com>
2022-07-14 14:54:30 +0300

    pybedtools games - continued

--- check_bedtools.py
+++ check_bedtools.py
@@ -3,20 +3,28 @@ import sys
 import pybedtools
 from pybedtools import BedTool
 
-def load_mutation_regions(annotations_file):
+def load_mutation_regions(annotations_file, squash_annotations=False):
     if annotations_file is not None:
         try:
-            annotations = BedTool(annotations_file)
+            annotations = pybedtools.example_bedtool(annotations_file)
+            annotations = annotations.sort() #TODO needed indeed?
             annotation_types = ['exon', 'CDS', 'five_prime_UTR', 'mRNA', 'three_prime_UTR', 'gene', 'ncRNA_gene',
                                 'lnc_RNA', 'tRNA', 'ncRNA', 'miRNA', 'snoRNA', 'snRNA', 'rRNA']
             for chrom in annotations.filter(lambda x: x.fields[2] == 'chromosome'):
                 print('----------------------------------------------------------------------')
                 print(f'chromosome {chrom.name} length is {chrom.length}')
-                for current_type in annotation_types:
-                    total_current_type_length = 0
-                    for element in annotations.filter(lambda x: x.fields[2] == current_type and x.chrom == chrom.chrom):
-                        total_current_type_length += element.length
-                    print(f'{current_type}\t{total_current_type_length}')
+                for strand in ['+', '-']:
+                    print(f'strand {strand} :')
+                    for current_type in annotation_types:
+                        total_current_type_length = 0
+                        annotations_of_type = annotations.filter(lambda x: x.fields[2] == current_type and x.chrom == chrom.chrom and x.strand == strand)
+                        annotations_of_type = annotations_of_type.saveas(f'annotations_of_type_{current_type}_strand_{strand}.bed') #, trackline='track name="a and b"')
+                        annotations_of_type = annotations_of_type.sort() #TODO needed indeed?
+                        if squash_annotations:
+                            annotations_of_type = annotations_of_type.merge()
+                        for element in annotations_of_type:
+                            total_current_type_length += element.length
+                        print(f'{current_type}\t{total_current_type_length}')
         except IOError:
             print("\nProblem reading annotation (BED/GFF) file.\n")
             sys.exit(1)
@@ -37,17 +45,23 @@ def cds_exon_intersection(annotations_file):
                 # print(f'exon+CDS\t{total_length}')
                 exon_intersect_cds = exon_elements.intersect(cds_elements)
                 exon_intersect_cds_df = exon_intersect_cds.to_dataframe()
+                exon_intersect_cds_df = exon_intersect_cds_df.drop_duplicates()
                 exon_intersect_cds_df.to_csv('exon_intersect_cds.csv')
 
                 # -v
-                exon_non_cds = exon_elements.intersect(cds_elements, v=True)
+                exon_elements = annotations.filter(lambda x: x.fields[2] == 'exon' and x.chrom == chrom.chrom)
+                cds_elements = annotations.filter(lambda x: x.fields[2] == 'CDS' and x.chrom == chrom.chrom)
+                # exon_non_cds = exon_elements.intersect(cds_elements, v=True)
+                exon_non_cds = exon_elements.subtract(cds_elements)
                 exon_non_cds_df = exon_non_cds.to_dataframe()
-                exon_non_cds_df.to_csv('exon_non_cds_df')
+                exon_non_cds_df.to_csv('exon_non_cds.csv')
 
                 # -u
+                exon_elements = annotations.filter(lambda x: x.fields[2] == 'exon' and x.chrom == chrom.chrom)
+                cds_elements = annotations.filter(lambda x: x.fields[2] == 'CDS' and x.chrom == chrom.chrom)
                 exon_intersect_cds_unique = exon_elements.intersect(cds_elements, u=True)
                 exon_intersect_cds_unique_df = exon_intersect_cds_unique.to_dataframe()
-                exon_intersect_cds_unique_df.to_csv('exon_intersect_cds_unique_df')
+                exon_intersect_cds_unique_df.to_csv('exon_intersect_cds_unique.csv')
 
 
                 print('----------------------------- Second try -----------------------------')
@@ -60,17 +74,24 @@ def cds_exon_intersection(annotations_file):
                 # print(f'exon+CDS\t{total_length}')
                 cds_intersect_exon = cds_elements.intersect(exon_elements)
                 cds_intersect_exon_df = cds_intersect_exon.to_dataframe()
+                cds_intersect_exon_df = cds_intersect_exon_df.drop_duplicates()
                 cds_intersect_exon_df.to_csv('cds_intersect_exon.csv')
 
                 # -v
-                cds_non_exon = cds_elements.intersect(exon_elements, v=True)
+                exon_elements = annotations.filter(lambda x: x.fields[2] == 'exon' and x.chrom == chrom.chrom)
+                cds_elements = annotations.filter(lambda x: x.fields[2] == 'CDS' and x.chrom == chrom.chrom)
+                # cds_non_exon = cds_elements.intersect(exon_elements, v=True)
+                cds_non_exon = cds_elements.subtract(exon_elements)
                 cds_non_exon_df = cds_non_exon.to_dataframe()
-                cds_non_exon_df.to_csv('cds_non_exon_df')
+                cds_non_exon_df.to_csv('cds_non_exon.csv')
+
 
                 # -u
+                exon_elements = annotations.filter(lambda x: x.fields[2] == 'exon' and x.chrom == chrom.chrom)
+                cds_elements = annotations.filter(lambda x: x.fields[2] == 'CDS' and x.chrom == chrom.chrom)
                 cds_intersect_exon_unique = cds_elements.intersect(exon_elements, u=True)
                 cds_intersect_exon_unique_df = cds_intersect_exon_unique.to_dataframe()
-                cds_intersect_exon_unique_df.to_csv('cds_intersect_exon_unique_df')
+                cds_intersect_exon_unique_df.to_csv('cds_intersect_exon_unique.csv')
 
                 break # TODO remove - currently we're checking only the first chromosome
         except IOError:
@@ -79,4 +100,6 @@ def cds_exon_intersection(annotations_file):
 
 
 if __name__ == "__main__":
-    cds_exon_intersection(sys.argv[1])
+    # args: /groups/itay_mayrose/adisivan/arabidopsis/ensemblgenomes/gff3/Arabidopsis_thaliana.TAIR10.53.gff3
+    load_mutation_regions(sys.argv[1], squash_annotations=True)
+    # cds_exon_intersection(sys.argv[1])
