6af898ae27d4bbafec5207c32d60bf1751c53057
Adi Sivan <adi.sivan05@gmail.com>
2022-09-27 18:57:31 +0300

    fixed bug

--- chromosome_simulator.py
+++ chromosome_simulator.py
@@ -121,16 +121,19 @@ class WindowUnit:
 
     def next_window(self, new_start: int = -1, new_end: int = -1):
         # TODO sanity check about start, end. Maybe consider N regions?
-        self.blocklist = {}  # np.zeros(end-start, dtype='<i4')
+        last_window_offset = self.end - self.original_end
+        new_start += (self.windows_start_offset + last_window_offset)
+        new_end += (self.windows_start_offset + last_window_offset)
         if new_start < 0 or new_end < 0 or not (self.end <= new_start < new_end):
             raise Exception(f'Illegal window positions. Start and end should be positive integers. '
                             f'The new window should come after the previous one and not overlap with it. '
                             f'Got the next values: start={new_start}, end={new_end}, '
                             f'while the previous values are: start={self.start}, end={self.end}')
-        self.windows_start_offset += (self.end - self.original_end)
+        self.windows_start_offset += last_window_offset
         self.start = new_start
         self.end = new_end
-        self.original_end = new_end
+        self.original_end = self.end
+        self.blocklist = {}  # np.zeros(end-start, dtype='<i4')
 
     def adjust_window(self, end_shift=0):
         # TODO sanity check about start, end. Maybe consider N regions?
