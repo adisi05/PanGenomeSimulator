0b14c5672c6233bb82c08b1872c2f38580010537
Adi Sivan <adi.sivan05@gmail.com>
2022-10-10 19:23:56 +0300

    get rid of deprecated files

--- deprecated/remote_debugging_init.py
+++
@@ -1,7 +0,0 @@
-# https://www.biostars.org/p/9472422/
-# https://github.com/sbslee/fuc
-import os
-import subprocess
-
-if __name__ == "__main__":
-    subprocess.run(["/groups/itay_mayrose/adisivan/PanGenomeSimulator/remote_debugging_init.sh"], shell=True)
