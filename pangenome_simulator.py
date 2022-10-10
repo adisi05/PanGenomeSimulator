8c5efdfb61946d5e05f06ab5b958440f5728af99
Adi Sivan <adi.sivan05@gmail.com>
2022-10-10 11:53:59 +0300

    reorder args

--- pangenome_simulator.py
+++ pangenome_simulator.py
@@ -25,35 +25,18 @@ def parse_args(raw_args=None):
                         help="Prefix for the output files (can be a path)")
     parser.add_argument('-c', type=float, required=False, metavar='coverage', default=10.0,
                         help="Average coverage, default is 10.0")
-    parser.add_argument('-e', type=str, required=False, metavar='error_model', default=None,
-                        help="Location of the file for the sequencing error model (omit to use the default)")
-    parser.add_argument('-E', type=float, required=False, metavar='Error rate', default=-1,
-                        help="Rescale avg sequencing error rate to this, must be between 0.0 and 0.3")
-    parser.add_argument('-tr', type=str, required=False, metavar='target.bed', default=None,
-                        help="Bed file containing targeted regions")
-    parser.add_argument('-dr', type=str, required=False, metavar='discard_regions.bed', default=None,
-                        help="Bed file with regions to discard")
-    parser.add_argument('-to', type=float, required=False, metavar='off-target coverage scalar', default=0.00,
-                        help="off-target coverage scalar")
     parser.add_argument('-m', type=str, required=False, metavar='model.p', default=None,
                         help="Mutation model pickle file")
     parser.add_argument('-M', type=float, required=False, metavar='avg mut rate', default=-1,
                         help="Rescale avg mutation rate to this (1/bp), must be between 0 and 0.3")
     parser.add_argument('-Mb', type=str, required=False, metavar='mut_rates.bed', default=None,
                         help="Bed file containing positional mut rates")
-    parser.add_argument('-N', type=int, required=False, metavar='min qual score', default=-1,
-                        help="below this quality score, replace base-calls with N's")
     parser.add_argument('-v', type=str, required=False, metavar='vcf.file', default=None,
                         help="Input VCF file of variants to include")
-    parser.add_argument('-ws', type=int, required=False, metavar='<int>', default=1000,
+    parser.add_argument('-ws', type=int, required=False, metavar='<int>', default=10000,
                         help='Window size of simulation. Choose an integer number larger than 10')
     parser.add_argument('--pe', nargs=2, type=int, required=False, metavar=('<int>', '<int>'), default=(None, None),
                         help='Paired-end fragment length mean and std')
-    parser.add_argument('--pe-model', type=str, required=False, metavar='<str>', default=None,
-                        help='empirical fragment length distribution')
-    parser.add_argument('--gc-model', type=str, required=False, metavar='<str>', default=None,
-                        help='empirical GC coverage bias distribution')
-    parser.add_argument('--bam', required=False, action='store_true', default=False, help='output golden BAM file')
     parser.add_argument('--vcf', required=False, action='store_true', default=False, help='output golden VCF file')
     parser.add_argument('--rng', type=int, required=False, metavar='<int>', default=-1,
                         help='rng seed value; identical RNG value should produce identical runs of the program, so '
@@ -61,13 +44,6 @@ def parse_args(raw_args=None):
                              'should all be the same.')
     parser.add_argument('--no-fastq', required=False, action='store_true', default=False,
                         help='bypass fastq generation')
-    parser.add_argument('--discard-offtarget', required=False, action='store_true', default=False,
-                        help='discard reads outside of targeted regions')
-    parser.add_argument('--force-coverage', required=False, action='store_true', default=False,
-                        help='[debug] ignore fancy models, force coverage to be constant')
-    parser.add_argument('--rescale-qual', required=False, action='store_true', default=False,
-                        help='Rescale quality scores to match -E input')
-    # TODO implement a broader debugging scheme for subclasses.
     parser.add_argument('-d', required=False, action='store_true', default=False, help='Activate Debug Mode')
     parser.add_argument('-newick', type=str, required=False, metavar='newick tree', help="Path to reference newick")
     parser.add_argument('-a', type=str, required=False, metavar='leaf.name', default=None, help='reference accession')
