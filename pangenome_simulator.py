46b4805a7ee0a2baa5118536ddd9a50d256003a0
Adi Sivan <adi.sivan05@gmail.com>
2022-10-10 18:26:27 +0300

    reorder pangenome_simulator.py

--- pangenome_simulator.py
+++ pangenome_simulator.py
@@ -13,6 +13,49 @@ from writers.fastq_file_writer import FastqFileWriter
 from writers.vcf_file_writer import VcfFileWriter
 
 
+def main(raw_args=None):
+    args = parse_args(raw_args)
+    print("Got the next args from the user:", args)
+    load_default_args(args)
+
+    # Single simulation
+    if not args.newick:
+        print("No phylogenetic tree supplied")
+        print("Generating sequence started")
+        start = time.time()
+        genome_simulator = GenomeSimulator(args)
+        genome_simulator.simulate()
+        end = time.time()
+        print("Done. Generating sequence took {} seconds.".format(int(end - start)))
+        return
+
+    # Tree simulation
+    t = ete3.Tree(args.newick, format=1)
+    internal_count = 0
+    for node in t.traverse("preorder"):
+        if not node.name:
+            internal_count += 1
+            node.name = f"internal_{internal_count}"
+    print("Using the next phylogenetic tree:\n", t.get_ascii(show_internal=True))
+    clear_previous_tree_output(args.o, t)
+    args.total_dist = tree_total_dist(t)
+
+    # Relate to one of the accessions (given by -a param) as the reference
+    args.root_to_ref_dist = set_ref_as_accession(args.a, t)
+
+    args.root_fasta = args.r
+
+    task_list = load_task_list(t, args)
+    if args.max_threads > 1:
+        generate_concurrently(args.max_threads, task_list)
+    else:
+        generate_sequentially(task_list)
+
+    print('================================')
+    # TODO remove all mut_bed files
+    # TODO remove all csv/whatever files now, and not before!
+
+
 def parse_args(raw_args=None):
     parser = argparse.ArgumentParser(description='PanGenomeSimulator',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter, )
@@ -48,46 +91,6 @@ def parse_args(raw_args=None):
     return parser.parse_args(raw_args)
 
 
-def main(raw_args=None):
-    args = parse_args(raw_args)
-    print("Using the next args:", args)
-    load_default_args(args)
-
-    if not args.newick:
-        print("No phylogenetic tree supplied")
-        print("Generating sequence started")
-        start = time.time()
-        genome_simulator = GenomeSimulator(args)
-        genome_simulator.simulate()
-        end = time.time()
-        print("Done. Generating sequence took {} seconds.".format(int(end - start)))
-        return
-
-    t = ete3.Tree(args.newick, format=1)
-    internal_count = 0
-    for node in t.traverse("preorder"):
-        if not node.name:
-            internal_count += 1
-            node.name = f"internal_{internal_count}"
-    print("Using the next phylogenetic tree:\n", t.get_ascii(show_internal=True))
-    clear_previous_tree_output(args.o, t)
-    args.total_dist = tree_total_dist(t)
-
-    # Relate to one of the accessions (given by -a param) as the reference
-    args.root_to_ref_dist = set_ref_as_accession(args.a, t)
-
-    args.root_fasta = args.r
-    task_list = load_task_list(t, args)
-    if args.max_threads > 1:
-        generate_concurrently(args.max_threads, task_list)
-    else:
-        generate_sequentially(task_list)
-
-    print('================================')
-    # TODO remove all mut_bed files
-    # TODO remove all csv/whatever files now, and not before!
-
-
 def load_default_args(args):
     args.name = "simulation"
     args.dist = 1
@@ -95,50 +98,53 @@ def load_default_args(args):
     args.parent_name = None
 
 
-def generate_sequentially(task_list):
-    print("TEST this is a single-process simulation")
-    for args in task_list:
-        generate_for_node(args)
+def clear_previous_tree_output(prefix, t):
+    for node in t.traverse("preorder"):
+        if node is t:
+            continue
+        else:
+            output_filenames = get_output_filenames(prefix, node.name)
+            for filename in output_filenames:
+                if os.path.exists(filename):
+                    os.remove(filename)
 
 
-def generate_concurrently(ncpu, task_list):
-    manager = multiprocessing.Manager()
-    pool = multiprocessing.Pool(processes=ncpu)
-    cond = manager.Condition()
-    pool.imap(process_handler, zip(task_list, repeat(cond)))
-    pool.close()
-    pool.join()
+def get_output_filenames(prefix, name):
+    res = []
+    res.extend(FastaFileWriter.get_output_filenames(prefix, name))
+    res.extend(FastqFileWriter.get_output_filenames(prefix, name))
+    res.extend(VcfFileWriter.get_output_filenames(prefix, name))
+    res.append(ANNOTATIONS_FILE_FORMAT.format(prefix + '_' + name))
+    return res
 
 
-def process_handler(params_with_cond):
-    simulation_params, cond = params_with_cond
-    curr_proc = multiprocessing.current_process()
-    print('TEST - current process:', curr_proc.name, curr_proc._identity)
-    # When ancestor is ready - then start simulating
-    ancestor_path = simulation_params.r
-    while not os.path.exists(ancestor_path):
-        print("TEST: ", curr_proc.name, 'checking if ancestor exists')
-        with cond:
-            cond.wait()
-            print("TEST: ", curr_proc.name, 'checking again if ancestor exists')
-    print("TEST: ", curr_proc, ", ancestor_path=", ancestor_path, "is ready")
-    generate_for_node(simulation_params)
-    with cond:
-        print("TEST: ", curr_proc, " has finished simulation for ", simulation_params.name)
-        cond.notify_all()
-
-    return "Done"
+def tree_total_dist(t):
+    total_dist = 0
+    for node in t.traverse("preorder"):
+        total_dist += node.dist
+    print("Total branches distance =", total_dist)
+    return total_dist
 
 
-def generate_for_node(args):
-    print("TEST inside generate_for_node, args.r=", args.r)
-    print("TEST inside generate_for_node, args.Mb=", args.Mb)
-    print("Generating sequence for taxon (node):", args.name)
-    start = time.time()
-    genome_simulator = GenomeSimulator(args)
-    genome_simulator.simulate()
-    end = time.time()
-    print("Done. Generating sequence for taxon {} took {} seconds.".format(args.name, int(end - start)))
+def set_ref_as_accession(accession, t):
+    root_to_ref_dist = 0
+    if accession:
+        ref_node = t & accession
+        t.set_outgroup(ref_node)
+        root_to_ref_dist = ref_node.dist
+        ref_node.delete()
+    return root_to_ref_dist
+
+
+def load_task_list(t, args):
+    task_list = []
+    for node in t.traverse("levelorder"):  # AKA breadth first search
+        if node is t:
+            continue  # This is the root - don't simulate for it
+        else:
+            node_params = get_node_args_for_simulation(node, args)
+            task_list.append(node_params)
+    return task_list
 
 
 def get_node_args_for_simulation(node, args):
@@ -158,54 +164,49 @@ def get_node_args_for_simulation(node, args):
     return new_args
 
 
-def get_output_filenames(prefix, name):
-    res = []
-    res.extend(FastaFileWriter.get_output_filenames(prefix, name))
-    res.extend(FastqFileWriter.get_output_filenames(prefix, name))
-    res.extend(VcfFileWriter.get_output_filenames(prefix, name))
-    res.append(ANNOTATIONS_FILE_FORMAT.format(prefix + '_' + name))
-    return res
+def generate_concurrently(ncpu, task_list):
+    print(f"Starting a multi-process simulation, number of processes: {ncpu}")
+    manager = multiprocessing.Manager()
+    pool = multiprocessing.Pool(processes=ncpu)
+    cond = manager.Condition()
+    pool.imap(process_handler, zip(task_list, repeat(cond)))
+    pool.close()
+    pool.join()
 
 
-def clear_previous_tree_output(prefix, t):
-    for node in t.traverse("preorder"):
-        if node is t:
-            continue
-        else:
-            output_filenames = get_output_filenames(prefix, node.name)
-            for filename in output_filenames:
-                if os.path.exists(filename):
-                    os.remove(filename)
+def process_handler(params_with_cond):
+    simulation_params, cond = params_with_cond
+    curr_proc = multiprocessing.current_process()
+    print('MULTIPROCESS LOG - current process:', curr_proc.name, curr_proc._identity)
+    # When ancestor is ready - then start simulating
+    ancestor_path = simulation_params.r
+    while not os.path.exists(ancestor_path):
+        print("MULTIPROCESS LOG: ", curr_proc.name, 'checking if ancestor exists')
+        with cond:
+            cond.wait()
+            print("MULTIPROCESS LOG: ", curr_proc.name, 'checking again if ancestor exists')
+    print("MULTIPROCESS LOG: ", curr_proc, ", ancestor_path=", ancestor_path, "is ready")
+    generate_for_node(simulation_params)
+    with cond:
+        print("MULTIPROCESS LOG: ", curr_proc, " has finished simulation for ", simulation_params.name)
+        cond.notify_all()
+
+    return "Done"
 
 
-def set_ref_as_accession(accession, t):
-    root_to_ref_dist = 0
-    if accession:
-        ref_node = t & accession
-        t.set_outgroup(ref_node)
-        root_to_ref_dist = ref_node.dist
-        ref_node.delete()
-    return root_to_ref_dist
+def generate_sequentially(task_list):
+    print("Starting a single-process simulation")
+    for args in task_list:
+        generate_for_node(args)
 
 
-def tree_total_dist(t):
-    total_dist = 0
-    for node in t.traverse("preorder"):
-        total_dist += node.dist
-    print("Total branches distance =", total_dist)
-    return total_dist
-
-
-def load_task_list(t, args):
-    task_list = []
-    for node in t.traverse("levelorder"):  # AKA breadth first search
-        if node is t:
-            continue  # This is the root - don't simulate for it
-        else:
-            node_params = get_node_args_for_simulation(node, args)
-            print("TEST, inside create_task_queue, node_params.name=", node_params.name)
-            task_list.append(node_params)
-    return task_list
+def generate_for_node(args):
+    print("Generating sequence for taxon (node):", args.name)
+    start = time.time()
+    genome_simulator = GenomeSimulator(args)
+    genome_simulator.simulate()
+    end = time.time()
+    print("Done. Generating sequence for taxon {} took {} seconds.".format(args.name, int(end - start)))
 
 
 if __name__ == "__main__":
