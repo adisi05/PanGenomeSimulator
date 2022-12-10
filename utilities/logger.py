b606dfc6f6b6479be9d813422a71186fe1672fce
Adi Sivan <adi.sivan05@gmail.com>
2022-12-10 10:15:49 +0200

    change logger methods

--- utilities/logger.py
+++ utilities/logger.py
@@ -9,9 +9,13 @@ class Logger:
     def set_concurrent(self, is_concurrent):
         self.is_concurrent = is_concurrent
 
-    def log(self, message: str):
-        if self.is_concurrent:
-            curr_proc = multiprocessing.current_process()
-            print(curr_proc.name, ' : ', message)
-        else:
-            print(message)
+    def message(self, message: str):
+        log_message(message, self.is_concurrent)
+
+
+def log_message(message: str, is_concurrent: bool = False):
+    if is_concurrent:
+        curr_proc = multiprocessing.current_process()
+        print(curr_proc.name, ' : ', message)
+    else:
+        print(message)
