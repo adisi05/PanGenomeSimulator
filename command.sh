c0f1c18a114835e80fd02938d29f949f7f3e1054
Adi Sivan <adi.sivan05@gmail.com>
2022-12-16 16:09:19 +0200

    rearrange bash scripts

---
+++ command.sh
@@ -0,0 +1,70 @@
+# !/bin/bash
+#PBS -S /bin/bash
+#PBS -j oe
+#PBS -r y
+#PBS -q itaym
+#PBS -v PBS_O_SHELL=bash,PBS_ENVIRONMENT=PBS_BATCH
+#PBS -N simulation_01
+#PBS -e /groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/simulation_01/logs
+#PBS -o /groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/simulation_01/logs
+#PBS -l select=ncpus=4:mem=40gb
+
+###############
+# Parameters: #
+###############
+code_dir=/groups/itay_mayrose/adisivan/PanGenomeSimulator
+job_dir=/groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/$PBS_JOBNAME
+accessions_vcf_files=$code_dir/vcf_inputs/intersection_*
+vcf_file=$code_dir/vcf_inputs/all_accessions.vcf
+#vcf_file=$job_dir/all.snp.filter.ref.variable.rename.vcf
+tree_file=$code_dir/vcf_inputs/tree.newick
+#tree_file=$job_dir/all.snp.filter.ref.variable.rename.min9.phy.treefile
+gff_file=/groups/itay_mayrose/adisivan/arabidopsis/ensemblgenomes/gff3/Arabidopsis_thaliana.TAIR10.53.gff3
+annotations_file=$job_dir/all_chroms_annotations.csv
+fasta_ref=/groups/itay_mayrose/adisivan/PanGenomeSimulator/test_arabidopsis/Arabidopsis_thaliana.TAIR10.dna.toplevel.fa
+model_file=$job_dir/mut_model.p
+
+########
+# Job: #
+########
+
+## 0. Initialization
+source ~/.bashrc
+hostname
+conda activate PanGenomeSimulator
+export PATH="$CONDA_PREFIX/bin:$PATH"
+cd $code_dir
+echo "Job Name: $PBS_JOBNAME"
+touch $job_dir/logs/out.txt
+if [[ ! -e $job_dir/logs/out.txt ]]; then
+    mkdir -p $job_dir/logs
+    touch $job_dir/logs/out.txt
+fi
+date >> $job_dir/logs/out.txt
+
+## 1. Process VCF files to infer tree and build a merged VCF file
+start=`date +%s`
+sh ./merge_accesion_vcfs_and_create_tree.sh $vcf_file $tree_file $accessions_vcf_files
+end=`date +%s`
+runtime=$((end-start))
+echo "VCF merging and tree generation took $runtime seconds" >> $job_dir/logs/out.txt 2>&1
+
+## 2. Process annotation file
+python process_annotations.py --gff $gff_file --csv $annotations_file
+
+## 3. Build model
+start=`date +%s`
+python /groups/itay_mayrose/adisivan/PanGenomeSimulator/mutation_model.py \
+-r $fasta_ref -v $vcf_file -o $model_file -a $annotations_file
+end=`date +%s`
+runtime=$((end-start))
+echo "Model building took $runtime seconds" >> $job_dir/logs/out.txt 2>&1
+
+## 4. Simulate pan-genome
+start=`date +%s`
+python /groups/itay_mayrose/adisivan/PanGenomeSimulator/pangenome_simulator.py \
+-r $fasta_ref -m $model_file -o $job_dir/output -a $annotations_file -t $tree_file --vcf --max-threads 3 \
+-R 150 --pe 250 20 -c 30
+end=`date +%s`
+runtime=$((end-start))
+echo "Simulation took $runtime seconds" >> $job_dir/logs/out.txt 2>&1
--- merge_accesion_vcfs_and_create_tree.sh
+++ merge_accesion_vcfs_and_create_tree.sh
@@ -1,49 +1,75 @@
 #!/bin/bash
 
+########################
+# Initialize variables #
+########################
 merge_input=""
 dir_name=""
+vcf_output=$1
+tree_output=$2
+# echo "vcf_output: $vcf_output";
+# echo "tree_output: $tree_output";
+
+########################
+# Files pre-processing #
+########################
+i=0
 for vcf_file in "$@"
 do
+  ((i++))
+  if [[ $i -lt 3 ]]; then
+    continue
+  fi
+  echo "Started pre-processing $vcf_file"
+
 	if [ -z "$dir_name" ];
 	then
 		dir_name=$(dirname ${vcf_file})
 	fi
-	
-    # echo "Started processing $vcf_file"
-    temp_file=$(basename ${vcf_file})
+
+  temp_file=$(basename ${vcf_file})
 	temp_file=${temp_file/#//temp_}
 	temp_file="$dir_name$temp_file"
-    cp ${vcf_file} ${temp_file}
-    # echo "Created $temp_file"
-    if [[ $temp_file == *.gz ]];
-    then
-        gzip -d ${temp_file}
-        temp_file=${temp_file%".gz"}
+  cp ${vcf_file} ${temp_file}
+  # echo "Created $temp_file"
+
+  if [[ $temp_file == *.gz ]];
+  then
+    gzip -d ${temp_file}
+    temp_file=${temp_file%".gz"}
 		# echo "Decompressed $temp_file"
-    fi
-    if [[ $temp_file != *.vcf ]];
-    then
-        printf "$temp_file is not a VCF file" >&2
+  fi
+
+  if [[ $temp_file != *.vcf ]];
+  then
+    printf "$temp_file is not a VCF file" >&2
 		rm temp_*
         exit 1
-    fi
-    bgzip ${temp_file}
-    cmprsd_temp_file=${temp_file/%/.gz}
-    # echo "Compressed $temp_file into $cmprsd_temp_file"
-    tabix -f -p vcf ${cmprsd_temp_file}
-    # echo "Indexed $cmprsd_temp_file"
-    merge_input="$merge_input $cmprsd_temp_file"
-	echo "Finished processing $vcf_file"
+  fi
+
+  bgzip ${temp_file}
+  cmprsd_temp_file=${temp_file/%/.gz}
+  # echo "Compressed $temp_file into $cmprsd_temp_file"
+
+  tabix -f -p vcf ${cmprsd_temp_file}
+  # echo "Indexed $cmprsd_temp_file"
+
+  merge_input="$merge_input $cmprsd_temp_file"
+	echo "Finished pre-processing $vcf_file"
 done
 
+###############
+# VCF merging #
+###############
 echo "Starting merging..."
-merge_output="$dir_name/all_accessions.vcf"
-bcftools merge --merge none ${merge_input} > ${merge_output}
-echo "Merged all vcf files into $merge_output"
+bcftools merge --merge none ${merge_input} > ${vcf_output}
+echo "Merged all vcf files into $vcf_output"
 rm ${dir_name}/temp_*
 # echo "Removed all temp files"
 
-echo "Starting tree creation..."
-tree_output="$dir_name/tree.newick"
-vk phylo tree upgma ${merge_output} > ${tree_output}
+###################
+# Tree generation #
+###################
+echo "Starting tree generation..."
+vk phylo tree upgma ${vcf_output} > ${tree_output}
 echo "Created tree $tree_output"
