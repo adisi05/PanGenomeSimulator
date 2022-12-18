0ad332d510dcbddb71ca776401adbc149fb727c8
Adi Sivan <adi.sivan05@gmail.com>
2022-12-18 10:08:59 +0200

    art_command.sh changes

--- art_command.sh
+++ art_command.sh
@@ -7,14 +7,15 @@
 #PBS -N simulation_05_x60
 #PBS -e /groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/simulation_05_x60/logs
 #PBS -o /groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/simulation_05_x60/logs
-#PBS -l select=ncpus=1:mem=10gb
+#PBS -l select=ncpus=1:mem=20gb
 
 ###############
 # Parameters: #
 ###############
 job_dir=/groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/$PBS_JOBNAME
 art_path=/groups/itay_mayrose/adisivan/PanGenomeSimulator/Simulator/ART/art_bin_MountRainier/art_illumina
-fasta_files=/groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/simulation_05/output_[A-Z]*.fasta
+fasta_dir=/groups/itay_mayrose/adisivan/PanGenomeSimulator/Panoramic/simulation_05
+fasta_files=$fasta_dir/output_[A-Z]*.fasta
 paired=-p
 read_length=150
 coverage=60
@@ -36,6 +37,7 @@ if [[ ! -e $job_dir/logs/out.txt ]]; then
     touch $job_dir/logs/out.txt
 fi
 date >> $job_dir/logs/out.txt
+cd $job_dir
 
 echo "Starting reads simulation" >> $job_dir/logs/out.txt 2>&1
 start=`date +%s`
@@ -43,8 +45,10 @@ for fasta_file in $fasta_files
 do
   echo "Started simulation for $fasta_file"
   fastq_file="${fasta_file%.fasta}_read"
-  $art_path $paired -i $fasta_file -l $read_length -f $coverage -o fastq_file -m $insert_size -s $insert_std
+  fastq_file="${fastq_file#$fasta_dir/}"
+  $art_path $paired -i $fasta_file -l $read_length -f $coverage -o $fastq_file -m $insert_size -s $insert_std
 	echo "Finished simulation for $fasta_file"
 done
-
+end=`date +%s`
+runtime=$((end-start))
 echo "reads simulation took $runtime seconds" >> $job_dir/logs/out.txt 2>&1
