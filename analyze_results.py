49dafad0ea37ed66466781e8615489c383a1d8ad
Adi Sivan <adi.sivan05@gmail.com>
2022-12-30 01:03:28 +0200

    Result analysis - work in progress

--- analyze_results.py
+++ analyze_results.py
@@ -9,7 +9,7 @@ NO_GENE = '0'
 GENE_PREF = 'transcript_'
 NOVEL_GENE = 'PanGene'
 ACCESSION_NAMES = ['A-lyrata', 'Cvi', 'C24', 'An-1', 'Col-0', 'Sha', 'Ler', 'Kyo', 'Eri']
-SIMULATOR_PAV_COLUMNS = ['gene_id'] + ACCESSION_NAMES
+SIMULATOR_PAV_COLUMNS = ['Name', 'Transcript'] + ACCESSION_NAMES
 PANORAMIC_PAV_COLUMNS = [f'{name}_{name}' for name in ACCESSION_NAMES] + [REF_LABEL, 'gene']
 parser = argparse.ArgumentParser(description='Plot and compare gene stats]',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter,)
@@ -33,23 +33,27 @@ for i in range(len(simulator_raw_pav_files)):
     raw_pav_df = pd.read_csv(simulator_raw_pav_files[i], index_col=0,
                              dtype={'gene_id': 'int', 'chrom': 'str', 'ref_start': 'int', 'ref_end': 'int'})
     legend_df = pd.read_csv(simulator_legend_files[i], index_col=0, dtype={'Name': 'str', 'ID': 'int'})
-    legend_df = legend_df.rename(columns={'ID': 'gene_id', 'oldName2': 'newName2'})
+    legend_df = legend_df.rename(columns={'ID': 'gene_id'})
     pav_df = pd.merge(raw_pav_df, legend_df, how='inner', on=['gene_id'])
+    pav_df[['Name', 'Transcript']] = pav_df.apply(lambda x: x['Name'].split('.'), axis=1, result_type='expand')
     pav_df = pav_df[SIMULATOR_PAV_COLUMNS]
     simulator_pav_dfs.append(pav_df)
 
 for i in range(len(panoramic_pav_files)):
-    missing_genes = []
+    missing_genes = {}
     with open(panoramic_pav_files[i]) as file:
-
+        simulator_pav_df = simulator_pav_dfs[i]
         tsv_file = csv.reader(file, delimiter="\t")
 
         ref_index = None
+        accession_indices = {}
         # i = 0
         for line in tsv_file:
 
             if not ref_index:
                 ref_index = line.index(REF_LABEL)
+                for a_name in ACCESSION_NAMES:
+                    accession_indices[a_name] = line.index(f'{a_name}_{a_name}')
                 continue
 
             if line[0].startswith(NOVEL_GENE):
@@ -57,9 +61,35 @@ for i in range(len(panoramic_pav_files)):
 
             line.pop(ref_index)
             if NO_GENE in line:
-                missing_genes.append(line[0])
-                # TODO test per accession against simulator_pav_dfs[i]
-    print(missing_genes)
-
-    # TODO compute which genes are also absent in the simulator PAVs.
-    #  Not sure if to do it accession-level or general-level.
+                gene_name = line[0].strip(GENE_PREF).split('.')[0]
+                simulator_result = simulator_pav_df[simulator_pav_df['Name'] == gene_name]
+                accession_dict = {}
+                if not simulator_result.empty:
+                    accession_dict['general'] = False in simulator_result[ACCESSION_NAMES].values[0]
+                    for a_name in ACCESSION_NAMES:
+                        if NO_GENE == line[accession_indices[a_name]]:
+                            accession_dict[a_name] = not simulator_result[a_name].item()
+                else:
+                    accession_dict['general'] = True
+                    for a_name in ACCESSION_NAMES:
+                        if NO_GENE == line[accession_indices[a_name]]:
+                            accession_dict[a_name] = True
+                missing_genes[gene_name] = accession_dict
+    print("Total missing genes by Panoramic:", len(missing_genes))
+    simulator_agree_count = 0
+    for _, v in missing_genes.items():
+        if v['general']:
+            simulator_agree_count += 1
+    print("The numbers of missing genes that are defined missing also by the simulator:", simulator_agree_count)
+    for a_name in ACCESSION_NAMES:
+        missing_genes_in_accession = 0
+        simulator_agree_count_accession = 0
+        for _, v in missing_genes.items():
+            if a_name in v:
+                missing_genes_in_accession += 1
+                if v[a_name]:
+                    simulator_agree_count_accession += 1
+        print("For accession:", a_name)
+        print("Total missing genes by Panoramic:", missing_genes_in_accession)
+        print("The numbers of missing genes that are defined missing also by the simulator:",
+              simulator_agree_count_accession)
